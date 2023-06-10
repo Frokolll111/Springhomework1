3124cf0037ea716db2c49f2e8fa1f1a4fd41f246
Frokolll111 <buther1433@gmail.com>
2023-06-10 14:52:07 +0700

    Commit

---
+++ src/test/java/pro/sky/Spring1/Spring1ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.Spring1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
