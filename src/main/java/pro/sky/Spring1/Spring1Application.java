931e7c23e5aab6333e6e5f42ce22a7cb11dc1c7f
Frokolll111 <buther1433@gmail.com>
2023-06-10 17:17:11 +0700

    Проверьте

--- src/main/java/pro/sky/Spring1/Spring1Application.java
+++ src/main/java/pro/sky/Spring1/Spring1Application.java
@@ -1,12 +1,11 @@
 package pro.sky.Spring1;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class Spring1Application {
 
 	public static void main(String[] args) {
+        System.setProperty("server.servlet.context-path=", "/store");
 		SpringApplication.run(Spring1Application.class, args);
 	}
 
