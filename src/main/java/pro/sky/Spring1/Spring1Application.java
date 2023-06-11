8f71ea01c4d46f8ce36fa18374f9251fe09759ee
Frokolll111 <buther1433@gmail.com>
2023-06-11 13:45:11 +0700

    Исправил

--- src/main/java/pro/sky/Spring1/Spring1Application.java
+++ src/main/java/pro/sky/Spring1/Spring1Application.java
@@ -5,8 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Spring1Application {
 
 	public static void main(String[] args) {
-        System.setProperty("server.servlet.context-path=", "/store");
 		SpringApplication.run(Spring1Application.class, args);
 	}
-
 }
